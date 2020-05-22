70dd5254f6466c769924b228d5a606642f14963a
Valerie Zykova <vizykova@gmail.com>
2020-05-22 16:48:14 +0300

    + read params from mt5

--- GapLocker/MODELS/PluginSettings.h
+++ GapLocker/MODELS/PluginSettings.h
@@ -7,16 +7,20 @@ struct PluginSettings
     struct Symbol
     {
         std::string Name;
-        double Points;
-        //to do
+        int Points;
+        time_t BeginTimeOffset;
+        time_t EndTimeOffset;
+
+        Symbol() : Name(""), Points(0), BeginTimeOffset(0), EndTimeOffset(SECONDS_IN_DAY) {}
     };
 
     std::string Groups;
-    std::string SessionTime;
+    time_t SessionBeginTimeOffset;
+    time_t SessionEndTimeOffset;
     std::map<int, std::shared_ptr<Symbol>> Symbols;
-    std::string SkipDays;
+    std::vector<boost::gregorian::greg_weekday> SkipDays;
 
     //needed?
     std::string Status;
-    bool DebugLogs;
+    bool DebugLogs = false;
 };
